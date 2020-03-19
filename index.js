8ba4c49db920b2b1f9066e05c39d754fc10b70b8
ElMayo <mic.l.meier@gmail.com>
2020-03-19 11:32:49 +0100

    exercise phonebook backend - step1

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+let contacts = [
+  {
+    name: "Arto Hellas",
+    number: "78-77-78-78",
+    id: 1
+  },
+  {
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+    id: 2
+  },
+  {
+    name: "Dan Abramov",
+    number: "12-43-234345",
+    id: 3
+  },
+  {
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+    id: 4
+  },
+  {
+    name: "Clark Kent",
+    number: "88-888-88-88-88",
+    id: 5
+  },
+  {
+    name: "Bruce Wayne",
+    number: "79*77*66*66",
+    id: 6
+  }
+];
+
+app.get("/", (req, res) => {
+  res.send("<h1>Phonebook</h1>");
+});
+
+app.get("/api/contacts", (req, res) => {
+  res.json(contacts);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
