9b9506a6b2989cbc85acdd800e341be7bfa1fc6e
ElMayo <mic.l.meier@gmail.com>
2020-03-21 09:48:03 +0100

    frontend production build

---
+++ build/precache-manifest.47432b6b2f1a2649b0f242316539cef6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80c514899954e2716af92af2bf965f71",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b8b5c9f65da7cf9f138",
+    "url": "/static/css/main.722026a5.chunk.css"
+  },
+  {
+    "revision": "113514b6a5b368e01e48",
+    "url": "/static/js/2.429c31ab.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.429c31ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b8b5c9f65da7cf9f138",
+    "url": "/static/js/main.62383730.chunk.js"
+  },
+  {
+    "revision": "ad9c1ae9b2cdeb3f7618",
+    "url": "/static/js/runtime-main.584b4e6a.js"
+  }
+]);
--- index.js
+++ index.js
@@ -43,6 +43,7 @@ const generateId = () => {
 // Middleware
 morgan.token("body", req => JSON.stringify(req.body));
 app.use(cors());
+app.use(express.static("build"));
 app.use(express.json());
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
